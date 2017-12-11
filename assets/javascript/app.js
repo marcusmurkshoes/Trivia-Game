c522f83dbb9f6179a8cac3fe8f876dc69ba27197
Marcus <therealthug09@aol.com>
2017-12-10 21:17:18 -0700

    Trivial Trivia 1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,45 @@
+
+// var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
 $(document).ready(function() {
 
+	var correctGuesses = 0;
+
+	correctGuesses = true;
+
+	var count=60;
+
+
+	var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
+
+
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+     return;
+  }
+
+ document.getElementById("timerCountdown").innerHTML=count + " secs"; // watch for spelling
+}
+
+$("#true").click(function() {
+
+	correctGuesses++;
+
+	correctGuesses = false, correctGuesses++;
+
+		console.log(correctGuesses);
+
+
+});
+
+
+
 	console.log("were ready bois");
+
 
 });
